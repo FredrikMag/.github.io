044a177094378abd8a282f9dca9981dd6b5ba0bf
FredrikMag <84189181+FredrikMag@users.noreply.github.com>
2021-05-27 17:36:47 +0200

    Added basic index files for WebGL testing

---
+++ index.js
@@ -0,0 +1,6 @@
+function simulateLogin(userName) {
+  var userNameField = document.getElementById("orderProductId_1");
+  // var value2 = userNameField.getElementsByClassName("from-group")[0];
+  // var value3 = value2.getElementsByTagName("input")[0];
+  userNameField.value = userName;
+}
